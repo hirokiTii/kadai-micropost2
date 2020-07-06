ff79ff278035a063c7b09dce6af60a31d65be4b2
EC2 Default User <hirokiki18@gmail.com>
2020-07-06 02:36:56 +0000

    add relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :microposts
+    has_many :relationships
+    has_many :followings, through: :relationships, source: :follow
+    has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follow_id"
+    has_many :followers, through: :reverse_of_relationships, source: :user
     has_many :favorites
 
     has_many :fav_posts, through: :favorites, source: :micropost
